04a196de7cb9b7027d9512a15a3ed9c51b9641b3
Arpit Rathore <87617789+arpitrathore055@users.noreply.github.com>
2021-11-25 16:50:28 +0530

    Update main.dart

--- Day 0 YouTubeUiClone/main.dart
+++ Day 0 YouTubeUiClone/main.dart
@@ -1,1 +1,412 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 0.0,
+                      ),
+                      child: Image.network(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWDMy_yf6XCxPRy2DmoaoZileC5Ef6UJFSHkpcGw1isa5SpT3q4CbRAvF0N2dhJxbifm8&usqp=CAU",
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 70,
+                      ),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.cast,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.account_circle,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 12.0),
+                        child: Chip(
+                          avatar: Icon(
+                            Icons.explore_outlined,
+                            size: 25,
+                          ),
+                          label: Text(
+                            "Explore",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      VerticalDivider(
+                        color: Colors.black,
+                        thickness: 1,
+                        indent: 20,
+                        endIndent: 0,
+                        width: 20,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 5.0,
+                        ),
+                        child: Chip(
+                          label: Text(
+                            "All",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 5.0),
+                        child: Chip(
+                          label: Text(
+                            "C++",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          right: 5.0,
+                        ),
+                        child: Chip(
+                          label: Text(
+                            "Lectures",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          right: 5.0,
+                        ),
+                        child: Chip(
+                          label: Text(
+                            "Comedy",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          right: 5.0,
+                        ),
+                        child: Chip(
+                          label: Text(
+                            "Live",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          right: 5.0,
+                        ),
+                        child: Chip(
+                          label: Text(
+                            "Puppy",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Stack(
+                      alignment: AlignmentDirectional.bottomEnd,
+                      children: [
+                        Image.network(
+                          "https://designhub.co/wp-content/uploads/2020/06/TitleImage2-364x205.png",
+                          width: double.infinity,
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Text(
+                              "10:00",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.all(9.0),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRntP9qX_Mf8Q14-Q1qdGhcVoaVSMZrzuJ83w&usqp=CAU"),
+                      ),
+                      title: Text(
+                        "How to make thumbnail!",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "My youtube School . 49K views . 10 hours ago",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Colors.black,
+                        ),
+                        iconSize: 25,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.bottomEnd,
+                      children: [
+                        Image.network(
+                          "https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg",
+                          width: double.infinity,
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Text(
+                              "15:03",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.all(9.0),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            "https://www.mockofun.com/wp-content/uploads/2019/12/circle-image.jpg"),
+                      ),
+                      title: Text(
+                        "How to make thumbnail(Explained)!",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "HARSH MALIK . 3K views . 21 hours ago",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Colors.black,
+                        ),
+                        iconSize: 25,
+                        onPressed: () {},
+                      ),
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.bottomEnd,
+                      children: [
+                        Image.network(
+                          "https://i.ytimg.com/vi/duJNVv9m2NY/maxresdefault.jpg",
+                          width: double.infinity,
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Text(
+                              "5:00",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.all(9.0),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDt-W1o9aZfJJKnAuAFVhGbb0SbYTtDZs7rQRp64yaWyf_DtQ5MMZ0g6367KYODSDoDFE&usqp=CAU"),
+                      ),
+                      title: Text(
+                        "How to make thumbnail in 2mins!",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "TANISHA SCHOOL . 103K views . 1 year ago",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.more_vert,
+                          color: Colors.black,
+                        ),
+                        iconSize: 25,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        //bottomNavigationBar: navbar(),
+      ),
+    );
+  }
+}
+
+class navbar extends StatefulWidget {
+  const navbar({Key? key}) : super(key: key);
+
+  @override
+  _navbarState createState() => _navbarState();
+}
+
+class _navbarState extends State<navbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              semanticLabel: "Home",
+              color: Colors.black,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.library_add,
+              semanticLabel: "Library",
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
