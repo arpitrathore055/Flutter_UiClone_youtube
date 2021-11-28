24958026cb088e7da4e479ca000ecfc999df4320
Arpit Rathore <arpitrathore009@gmail.com>
2021-11-28 20:02:29 +0530

    Update main.dart
    Day 3 of creating YouTubeUiClone involves assembling of different sections through routes and navigation.

--- Day 3 YouTubeUiClone/main.dart
+++ Day 3 YouTubeUiClone/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: homepage(),
+    );
+  }
+}
 
